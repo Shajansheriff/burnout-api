6b755ab8b2dba88eb709085260a1fec7d0609d19
Shajahan A <shajansheriff@gmail.com>
2016-08-09 03:14:08 +0530

    Postgres Setup and Added Procfile (#3)

--- burnout/settings.py
+++ burnout/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'burnout.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'burnout',
+        'USER': 'shahjahan',
+        'PASSWORD': 'hello',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
